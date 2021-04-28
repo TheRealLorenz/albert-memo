a3ba17f752118b9839d49eeaf2e07297842b5d79
TheRealLorenz <lollobellina@gmail.com>
2021-04-28 14:54:53 +0200

    Fix injection issue and update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-if [ ! -d ~/.local/share/albert/org.albert.extension.python/modules/ ]; then
-    mkdir -p ~/.local/share/albert/org.albert.extension.python/modules/
+if [ ! -d ~/.local/share/albert/org.albert.extension.python/modules/albert-memo ]; then
+    mkdir -p ~/.local/share/albert/org.albert.extension.python/modules/albert-memo 2>/dev/null
 fi
 chmod +x add-item
 chmod +x rm-item
-cp -r ../albert-memo ~/.local/share/albert/org.albert.extension.python/modules/
+cp -r ./{add-item,rm-item,__init__.py,*.png} ~/.local/share/albert/org.albert.extension.python/modules/albert-memo
